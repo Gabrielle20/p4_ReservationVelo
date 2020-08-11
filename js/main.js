b654ad8989003a847065755ec0d6a0048b692f1e
Gabrielle Caeiro <gabriellecaeiro@gmail.com>
2020-08-11 11:16:35 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -3,4 +3,4 @@ new DataHandler("adresseAPI.json");
 new ComposantTest("monTest", document.body);
 new ComposantTest("monTest2", document.body);
 new ComposantTest2("gtzrheny", document.body);
-new Diaporama("diaporama",document.body, ["images/diapo1.jpg","images/diapo2.jpg","images/velo1.jpg"], 2000);
+new Diaporama("diaporama",document.body, ["images/diapo1.jpg","images/diapo2.jpg","images/diapo3.jpg"], 5000);

bacef0111ef52850a23fe645e70548c426c7bcad
Gabrielle Caeiro <gabriellecaeiro@gmail.com>
2020-08-21 11:34:52 +0200

    diapo buttons

--- js/diaporama.js
+++ js/diaporama.js
@@ -13,23 +13,68 @@ class Diaporama extends Composant{
     this.currentSlideId = -1;
     this.images         = images;
     this.timeout        = timeout;
-
-    this.tempo = setInterval(this.nextSlide.bind(this), this.timeout);
+    this.tempo          = setInterval(this.nextSlide.bind(this), this.timeout);
+    this.playing        = true;
     
-    this.slide     = new Slide("imageSlide", this.DOM);
-    this.prevtBt   = new SlideButton("Prev", this.DOM);
-    this.playPause = new SlideButton("Play/Pause", this.DOM);
-    this.nextBt    = new SlideButton("Next", this.DOM);
-
+    this.slide       = new Slide("imageSlide", this.DOM);
+    this.prevtBt     = new SlideButton("Prev", this.DOM, "prev", this.prevSlide.bind(this));
+    this.playPauseBt = new SlideButton("Play/Pause", this.DOM, "pause", this.playPause.bind(this));
+    this.nextBt      = new SlideButton("Next", this.DOM, "next", this.nextSlide.bind(this));
     this.nextSlide();
   }
 
   nextSlide(){
+    // console.log(this);
     this.currentSlideId++;
     if (this.currentSlideId >= this.images.length) this.currentSlideId = 0;
     console.log(this.currentSlideId);
     this.slide.update(this.images[this.currentSlideId]);
   }
+
+  prevSlide(){
+    this.currentSlideId--;
+    if (this.currentSlideId <= this.images.length) this.currentSlideId = 0;
+    console.log(this.currentSlideId);
+    this.slide.update(this.images[this.currentSlideId]);
+  }
+
+
+  playPause(){
+    console.log(this);
+    this.playing = !this.playing;
+    if(this.playing){
+      //arreter la tempo   
+      clearInterval(this.tempo);
+      delete this.tempo;
+      clearTimeout();
+      delete this.timeout;
+      this.playPauseBt.update("play");
+    }
+    else {
+      this.tempo = setInterval(this.nextSlide.bind(this), this.timeout);
+      this.playPauseBt.update("pause");
+    }
+
+  }
+
+
+
+  //Touches du clavier
+  window.addEventListener('keyup', (e)) => {
+    switch (e.key) {
+      case 'ArrowLeft': 
+        prevSlide = prevSlide.bind(this);
+        break;
+
+      case 'ArrowRight':
+        nextSlide = nextSlide.bind(this);
+        break;
+    }
+  }
+
+  // onkeypress
+  // onkeydown
+  // onkeyup
 
   
 }
--- js/slideButton.js
+++ js/slideButton.js
@@ -6,50 +6,13 @@ class SlideButton extends Composant{
    * @param {string}      name       le nom du composant
    * @param {HTMLElement} domTarget  l'endroit où injecter le composant
    */
-  constructor(name, domTarget){
-    super(name, domTarget, "button");
-    this.DOM.innerHTML = name;
-    
-    this.prevtBt   = new SlideButton("Prev", this.DOM);
-    this.playPause = new SlideButton("Play/Pause", this.DOM);
-    this.nextBt    = new SlideButton("Next", this.DOM);
-
-    this.render();
-    this.DOM.onclick = this.click.bind(this);
+  constructor(name, domTarget, classname, action){
+    super(name, domTarget, "slideButton");
+    this.DOM.className = classname;
+    this.DOM.onclick = action;
   }
 
-  render(){
-   if(this.nextBt.onclick) return this.nextSlideBtn();
-   if(this.playPause.onclick) return this.playPauseBtn();
-   if(this.prevtBtn.onclick) return this.prevSlideBtn();
+  update(classname){
+    this.DOM.className = classname;
   }
-
-  click(){
-    console.log(this);
-        this.state = !this.state;
-        this.render();
-  }
-
-  nextSlideBtn(){
-    this.currentSlideId++;
-    console.log(this.currentSlideId);
-    this.slide.update(this.images[this.currentSlideId]);
-  }
-
-  playPauseBtn(){
-    this.currentSlideId++;
-    console.log(this.currentSlideId);
-    this.slide.update(this.images[this.currentSlideId]);
-  }
-
-  prevSlideBtn(){
-    this.currentSlideId++;
-    console.log(this.currentSlideId);
-    this.slide.update(this.images[this.currentSlideId]);
-  }
-
-  // onkeypress
-  // onkeydown
-  // onkeyup
-
 }

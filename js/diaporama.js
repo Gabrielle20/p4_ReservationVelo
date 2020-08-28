dc9748469c4e12cc84b24adfb4c113b9040c6c2b
Gabrielle Caeiro <gabriellecaeiro@gmail.com>
2020-08-28 11:44:43 +0200

    map-1

--- js/diaporama.js
+++ js/diaporama.js
@@ -10,21 +10,38 @@ class Diaporama extends Composant{
   constructor(name, domTarget, images, timeout){
     super(name, domTarget, "diaporama");
     
+    console.log(this);
+    
     this.currentSlideId = -1;
     this.images         = images;
     this.timeout        = timeout;
-    this.tempo          = setInterval(this.nextSlide.bind(this), this.timeout);
     this.playing        = true;
     
     this.slide       = new Slide("imageSlide", this.DOM);
     this.prevtBt     = new SlideButton("Prev", this.DOM, "prev", this.prevSlide.bind(this));
     this.playPauseBt = new SlideButton("Play/Pause", this.DOM, "pause", this.playPause.bind(this));
     this.nextBt      = new SlideButton("Next", this.DOM, "next", this.nextSlide.bind(this));
+    
+    this.restart();
     this.nextSlide();
+    
+
+      //Touches du clavier
+    window.addEventListener('keyup', (e) => {
+      switch (e.key) {
+        case 'ArrowLeft': 
+          this.prevSlide();
+          break;
+  
+        case 'ArrowRight':
+          this.nextSlide();
+          break;
+      }
+    })
   }
 
   nextSlide(){
-    // console.log(this);
+    this.restart();
     this.currentSlideId++;
     if (this.currentSlideId >= this.images.length) this.currentSlideId = 0;
     console.log(this.currentSlideId);
@@ -33,44 +50,33 @@ class Diaporama extends Composant{
 
   prevSlide(){
     this.currentSlideId--;
-    if (this.currentSlideId <= this.images.length) this.currentSlideId = 0;
+    if (this.currentSlideId <= 0) this.currentSlideId = this.images.length -1;
     console.log(this.currentSlideId);
     this.slide.update(this.images[this.currentSlideId]);
   }
 
 
   playPause(){
-    console.log(this);
     this.playing = !this.playing;
-    if(this.playing){
+    if(!this.playing){
       //arreter la tempo   
       clearInterval(this.tempo);
-      delete this.tempo;
       clearTimeout();
-      delete this.timeout;
       this.playPauseBt.update("play");
     }
     else {
-      this.tempo = setInterval(this.nextSlide.bind(this), this.timeout);
+      this.restart();
       this.playPauseBt.update("pause");
     }
 
   }
 
-
-
-  //Touches du clavier
-  window.addEventListener('keyup', (e)) => {
-    switch (e.key) {
-      case 'ArrowLeft': 
-        prevSlide = prevSlide.bind(this);
-        break;
-
-      case 'ArrowRight':
-        nextSlide = nextSlide.bind(this);
-        break;
-    }
+  restart(){
+    clearInterval(this.tempo);
+    this.tempo = setInterval(this.nextSlide.bind(this), this.timeout);
   }
+
+
 
   // onkeypress
   // onkeydown
--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var demo = {};
-new DataHandler("adresseAPI.json");
+new DataHandler("https://api.jcdecaux.com/vls/v1/stations?contract={contract_name}&apiKey={api_key}");
 new ComposantTest("monTest", document.body);
 new ComposantTest("monTest2", document.body);
 new ComposantTest2("gtzrheny", document.body);
-new Diaporama("diaporama",document.body, ["images/diapo1.jpg","images/diapo2.jpg","images/diapo3.jpg"], 5000);
+new Diaporama("diaporama",document.querySelector("#main-banner"), ["images/diapo1.jpg","images/diapo2.jpg","images/diapo3.jpg"], 5000);
+new Map("map", document.body);
+
+
+// GET https://api.jcdecaux.com/vls/v1/stations?contract={contract_name}&apiKey={api_key}

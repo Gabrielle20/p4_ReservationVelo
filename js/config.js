44840f71fe1ccb8fabfb86ce4d3bb2e8dacd5e05
Gabrielle Caeiro <gabriellecaeiro@gmail.com>
2020-09-04 11:31:44 +0200

    map 2

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const config = {
   apiJCDecaux : "lmklklklkmlk",
-  leafletToken : "kljkljkljkljkljklj"
+  leafletToken : "pk.eyJ1IjoiZ2FicmllbGxlY3AiLCJhIjoiY2tlbndmZjgwM2U1aTJ5bHR3Y2duN29nNiJ9.Eq2OKB_Ve6-L3_2mamC6OQ"
 }
--- js/mapLeaflet.js
+++ js/mapLeaflet.js
@@ -8,20 +8,34 @@ class MapLeaflet {
     constructor(name){
         // super(name, domTarget, "map");
         this.name = name
-        this.mymap = L.map('mapid').setView([51.505, -0.09], 13);
-        L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${config.leafletToken}`, {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: 'your.mapbox.access.token'
-            }
-        ).addTo(this.mymap);
+        this.mymap = L.map('mapid').setView([44.84029065139799, -0.5657958984375], 13);
+        L.tileLayer('https://api.maptiler.com/maps/streets/tiles.json?key=5b828jhpiWGgUYq6LMOe'), {
+            attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright"target="_blank">&copy; OpenStreetMap contributors</a>',
+        }).addTo(this.mymap);
     }
 }
 
-/*
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-*/
+// L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${config.leafletToken}`, {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: 'your.mapbox.access.token',
+//     }).addTo(this.mymap);
+
+
+//this.marker = L.marker([44.84029065139799, -0.5657958984375]).addTo(this.mymap);
+
+// this.circle = L.circle([51.508, -0.11], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(mymap);
+
+
+// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+// circle.bindPopup("I am a circle.");
+// polygon.bindPopup("I am a polygon.");

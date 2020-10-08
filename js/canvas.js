d1a7b7906db995e64eba12981360af9020ef22ef
Lionel <fullstackbeaver@gmail.com>
2020-10-08 11:02:08 +0200

    séance du 8 octobre

--- js/canvas.js
+++ js/canvas.js
@@ -1,68 +1,113 @@
 class Canvas extends Composant {
-    constructor(domTarget){
-        super("canvas", domTarget, "canvas");
-        veloReservation = this;
-        // this.mouseMvt = new MouseEvent(click);
-        this.clickX;
-        this.clickY;
-        this.clickDrag;
-        this.paint;
-        this.mousedown();
-        this.mouseleave();
-        this.addClick();
-        this.redraw();
+  constructor(domTarget) {
+    super("canvas", domTarget, "canvas");
+    this.DOM.height        = "500";
+    this.DOM.width         = "200";
+    this.clickDrag         = [];
+    this.context           = this.DOM.getContext('2d');
+    this.paint             = false;
+    this.previousClickX    = null;
+    this.previousClickY    = null;
+    this.rect              = this.DOM.getBoundingClientRect();
+
+    this.DOM.onmousedown   = this.mouseDown.bind(this);
+    this.DOM.onmousemove   = this.mouseMove.bind(this);
+    this.DOM.onmouseup     = this.mouseleave.bind(this);
+
+    this.DOM.ontouchcancel = this.mouseleave.bind(this);
+    this.DOM.ontouchend    = this.mouseleave.bind(this);
+    this.DOM.ontouchleave  = this.mouseleave.bind(this);
+    this.DOM.ontouchmove   = this.handleMove.bind(this);
+    this.DOM.ontouchstart  = this.handleStart.bind(this);
+  }
+
+  handleStart(evt) {
+    evt.preventDefault();
+    this.paint = true;
+    const position = this.convertCoordTouch(evt);
+    this.draw(position.x, position.y);
+  }
+  handleMove(evt) {
+    evt.preventDefault();
+    if (!this.paint) return;
+    const position = this.convertCoordTouch(evt);
+    this.draw(position.x, position.y);
+  }
+
+  // Canvas Souris
+  mouseDown(e) {
+    const position = this.convertCoordMouse(e);
+    this.paint = true;
+    this.draw(position.x, position.y);
+  }
+
+  mouseMove(e) {
+    if (!this.paint) return;
+    const position = this.convertCoordMouse(e);
+    this.draw(position.x, position.y);
+  }
+
+  convertCoordTouch(e) {
+    return {
+      x: e.changedTouches[0].pageX - this.rect.left,
+      y: e.changedTouches[0].pageY - this.rect.top
     }
-
-    // Canvas Souris
-    mousedown(e) {
-        var mouseX = e.pageX - this.offsetLeft;
-        var mouseY = e.pageY - this.offsetTop;
-
-        paint = true;
-        addClick(e.pageX - this.offsetLeft, e.pageY - offsetTop);
-        redraw();
+  }
+  convertCoordMouse(e) {
+    return {
+      x: e.clientX - this.rect.left,
+      y: e.clientY - this.rect.top
     }
+  }
 
-    mouseleave() {
-        paint = false;
+  draw(mouseX, mouseY) {
+    console.log(mouseX, mouseY);
+    if (this.previousClickX !== null) {
+      this.context.beginPath();
+      this.context.strokeStyle = 'black';
+      this.context.lineWidth = 1;
+
+      this.context.moveTo(this.previousClickX, this.previousClickY);
+      this.context.lineTo(mouseX, mouseY);
+      this.context.stroke();
+      this.context.closePath();
+    };
+    this.previousClickX = mouseX;
+    this.previousClickY = mouseY;
+  }
+
+  mouseleave() {
+    this.paint = false;
+    this.previousClickX = null;
+    this.previousClickY = null;
+  }
+
+  addClick(x, y, dragging = null) {
+    this.clickX = x;
+    this.clickY = y;
+    this.clickDrag.push(dragging);
+  }
+
+  redraw() {
+    this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height); //Nettoie le canvas
+
+    this.context.strokeStyle = "#df4b26";
+    this.context.lineJoin = "round";
+    this.context.lineWidth = 5;
+
+    for (var i = 0; i < this.clickX.length; i++) {
+      this.context.beginPath();
+      if (this.clickDrag[i] && i) {
+        this.context.moveTo(this.clickX[i - 1], this.clickY[i - 1]);
+      }
+      else {
+        this.context.moveTo(this.clickX[i] - 1, this.clickY[i]);
+      }
+
+      this.context.lineTo(this.clickX[i], this.clickY[i]);
+      this.context.closePath();
+      this.context.stroke();
     }
-
-    addClick (x, y, dragging) {
-        clickX.push(x);
-        clickY.push(y);
-        clickDrag.push(dragging);
-    }
-
-    redraw() {
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height); //Nettoie le canvas
-
-        context.strokeStyle = "#df4b26";
-        context.lineJoin = "round";
-        context.lineWidth = 5;
-
-        for (var i=0; i < clickX.length; i++) {
-            context.beginPath();
-            if(clickDrag[i] && i) {
-                context.moveTo(clickX[i-1], clickY[i-1]);
-            }
-            else  {
-                context.moveTo(clickX[i]-1, clickY[i]);
-            }
-
-            context.lineTo(clickX[i], clickY[i]);
-            context.closePath();
-            context.stroke();
-        }
-    }
-
-    //Canvas Tactile
-    // startup() {
-    //     var el = document.getElementsByTagName("canvas")[0];
-    //     el.addEventListener("touchstart", handleStart, false);
-    //     el.addEventListener("touchend", handleEnd, false);
-    //     el.addEventListener("touchcancel", handleCancel, false);
-    //     el.addEventListener("touchleave", handleLeave, false);
-    //     el.addEventListener("touchmove", handleMove, false);
-    //   }
+  }
 }
 

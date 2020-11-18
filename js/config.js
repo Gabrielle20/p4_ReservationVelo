b271f9dd4c9bf29ecdaad71449aa83a909381600
Gabrielle Caeiro <gabriellecaeiro@gmail.com>
2020-11-18 17:05:04 +0100

    18112020 Modifs 1

--- js/config.js
+++ js/config.js
@@ -2,8 +2,6 @@ const config = {
   keyJCDecaux     : "6be18ab02ece9a570a4d380242601a4497063e63",
   contractJCDecaux: "lyon",
   leafletToken    : "pk.eyJ1IjoiZ2FicmllbGxlY3AiLCJhIjoiY2tlbndmZjgwM2U1aTJ5bHR3Y2duN29nNiJ9.Eq2OKB_Ve6-L3_2mamC6OQ",
-  // "lat"           : 44.84029065139799,
-  // "long"          : -0.5657958984375
   "lat"           : 45.7605982,
   "long"          : 4.8229873
 }
